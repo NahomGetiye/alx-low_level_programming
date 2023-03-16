838a3dd8d0b63362409a3e249c284eb811ef4088
NahomGetiye <nahomgetiye000@gmail.com>
2023-03-16 13:19:54 -0700

    hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - this is a c program to print using puts function
+* Return: 0 (Success)
+*/
+int main(void)/*this is the main function*/
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}

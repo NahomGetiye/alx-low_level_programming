167b3852ea5350346ae42aeab1a2938115ed445b
NahomGetiye <nahomgetiye000@gmail.com>
2023-03-16 13:30:22 -0700

    fix printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - this is a c program to print using puts function
+* main - this is a c program to print using printf function
 * Return: 0 (Success)
 */
 int main(void)/*this is the main function*/
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }

6ebfad05b3306af3b3d4f43f328379fe70b7223d
NahomGetiye <nahomgetiye000@gmail.com>
2023-03-16 13:13:07 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
 */
 int main(void)/*this is the main function*/
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }

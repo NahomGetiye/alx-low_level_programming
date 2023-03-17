1eb16e310977d85b63f9b244a13fc82395af30ad
NahomGetiye <nahomgetiye000@gmail.com>
2023-03-17 13:58:50 -0700

    numz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - numbers from 0 to 9
+  * Return: returns zero
+  */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar (n);
+		n++;
+	}
+	putchar ('\n');
+	return (0);
+}

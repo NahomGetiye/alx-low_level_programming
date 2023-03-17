ed0f1e318f7d15a66163fa3a266b61943a606115
NahomGetiye <nahomgetiye000@gmail.com>
2023-03-17 13:08:34 -0700

    reverse order

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - contains the alphabetical order
+  *Return: return the value
+  */
+
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar (n);
+		n--;
+		putchar ('\n');
+	}
+return (0);
+}

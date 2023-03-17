8f755aef82a47b8e698af238b3fb879b9a1db66e
NahomGetiye <nahomgetiye000@gmail.com>
2023-03-17 14:41:55 -0700

    combination advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+  * main - combinational numbers
+  * Return: returns the value
+  */
+int main(void)
+{
+	int m,n;
+
+	for (m = '0'; m < '9'; m++)
+	{
+		for (n = m+1; n <= '9'; n++)
+		{
+			if (n != m)
+			{
+				putchar (m);
+				putchar (n);
+				if (m == '8' && n == '9')
+				{
+					continue;
+				}
+				putchar (',');
+				putchar (' ');
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}

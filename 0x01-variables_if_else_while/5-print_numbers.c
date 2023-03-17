bc89290eb70984fa9e977ecbcc23a2a280a12663
NahomGetiye <nahomgetiye000@gmail.com>
2023-03-17 13:50:47 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - Numbers zero to ten
+  * Return: return zero
+  */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	
+	printf("\n");
+	return (0);
+}

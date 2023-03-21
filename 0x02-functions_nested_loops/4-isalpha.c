1f6a3012b5b8da3594906b4633b7710fd88fc7c3
NahomGetiye <nahomgetiye000@gmail.com>
2023-03-21 14:08:34 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,11 +6,7 @@
   */
 int _isalpha(int c)
 {
-	if (c == 59)
-	{
-		return (0);
-	}
-	else if (c >= 97 || c <= 122 || c >= 65 || c <= 90)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}

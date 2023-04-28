b5af1d9322f1084b9d6c34be731d0eb32ac6d8d0
NahomGetiye <nahomgetiye000@gmail.com>
2023-04-28 11:17:32 -0700

    story

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
+void first(void) __attribute__ ((constructor));
+
 /**
- * printMessage - Prints a message to the console.
+ * first - prints a sentence before the main
+ * function is executed
  */
-void printMessage(void)
+void first(void)
 {
-printf("You're beat! and yet, you must allow,\n"
-"I bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }

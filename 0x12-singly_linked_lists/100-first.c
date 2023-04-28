780f089bcd54518c2b85a2b98b6f4f4689b08a34
NahomGetiye <nahomgetiye000@gmail.com>
2023-04-28 11:13:41 -0700

    story

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * printMessage - Prints a message to the console.
+ */
+void printMessage(void)
+{
+printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
+}

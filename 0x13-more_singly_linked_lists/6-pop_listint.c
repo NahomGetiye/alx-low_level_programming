125c542852fc7eff92472f0addd2285f7b2d098a
NahomGetiye <nahomgetiye000@gmail.com>
2023-04-30 11:51:00 -0700

    linked list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * pop_listint - deletes the head node of a linked list
+ * @head: the first element in the linked list
+ * Return: returns the value
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int num;
+
+	if (!head || !*head)
+		return (0);
+
+	num = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (num);
+}

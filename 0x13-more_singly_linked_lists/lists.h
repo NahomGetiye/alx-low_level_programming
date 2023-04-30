b05cb871e51880144735df174b79589d3b582913
NahomGetiye <nahomgetiye000@gmail.com>
2023-04-30 02:02:54 -0700

    print list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,5 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -11,8 +10,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
